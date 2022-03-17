ff3b343f5d21a2727a958af969b0c2763c2c7d63
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-17 10:14:00 +0530

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -2,17 +2,19 @@ let fetch = require('node-fetch')
 
 let handler = m => m
 
-handler.before = async (m) => {
+handler.before = async (m, { conn }) => {
     let chat = db.data.chats[m.chat]
     if (chat.simi && !chat.isBanned) {
         if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(API('xteam', '/simsimi', { kata: encodeURIComponent(m.text) }, 'APIKEY'))
-        if (!res.ok) return m.reply(`${res.status} ${res.statusText}`)
+        let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'en' }))
+        if (!res.ok) return m.reply(error)
         let json = await res.json()
-        if (!json.status) return m.reply(require('util').format(json))
-        m.reply('Simi: ' + json.jawaban)
+        if (m.quoted && m.quoted.sender === conn.user.jid) {
+         if (json.success == "panget si nica") return m.reply ("BAKA !")
+         m.reply(json.success)
         return !0
+      }
     }
     return !0
 }
