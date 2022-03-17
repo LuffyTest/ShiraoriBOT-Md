ec4394871ee0c6c94edb11303575891add6485f8
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-17 18:31:51 +0530

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,14 +2,14 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  if (user.registered === true) throw `You are already registered\nWant to re-register? ${usedPrefix}unreg <SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `example:\n*${usedPrefix + command}name.age*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Name cannot be empty'
+  if (!age) throw 'Age can\'t be empty'
   age = parseInt(age)
-  if (age > 30) throw 'Lu dah tua anjing'
-  if (age < 5) throw 'Mau diban?'
+  if (age > 30) throw 'You\'re an old dog 😒'
+  if (age < 5) throw 'Want to get banned?'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
@@ -17,19 +17,19 @@ let handler = async function (m, { text, usedPrefix, command }) {
   let sn = createHash('md5').update(m.sender).digest('hex')
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
   m.reply(`
-━━━━ 「 *Successful Registration* 」━━━━
+━━━━ 「 * Registration Successful* 」━━━━
 
 ╭─• 〘 INFO 〙
-│➥ Nama: ${name}
-│➥ Umur: ${age} Tahun
-│➥ Hadiah: ${prems.includes(who.split`@`[0]) ? '✅ Silahkan chat owner untuk claim hadiah' : '❌ Bukan User Premium'}
+│➥ Name: ${name}
+│➥ Age: ${age} Years
+│➥ Present: ${prems.includes(who.split`@`[0]) ? 'Please chat with the owner to claim the prize ✅' : '❌ Not a Premium User'}
 ╰──────•
 
-Ketik ${usedPrefix} untuk mendapatkan SERIAL NUMBER\n
-sn digunakan untuk unregister
+Type ${usedPrefix} to get the SERIAL NUMBER\n
+sn is used to unregister
 `.trim())
 }
-handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
+handler.help = ['daftar', 'register'].map(v => v + ' <name>.<age>')
 handler.tags = ['xp', 'main']
 
 handler.command = /^(daftar|reg(ister)?)$/i
