b524299f614e05835a1762dc4e85822e456aa221
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-19 14:31:32 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,14 +1,16 @@
 let yts = require('yt-search')
-let handler = async (m, { text }) => {
-  if (!text) throw 'Cari apa?'
+
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `Use:\n${usedPrefix + command} <teks>\n\nEX:\n${usedPrefix + command} One Piece`
   let results = await yts(text)
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-*${v.title}* (${v.url})
-Duration: ${v.timestamp}
-Uploaded ${v.ago}
-${v.views} views
+*🔮 Title: ${v.title}* 
+*🔅 Url:* \`\`\`${v.url}\`\`\`
+*💠 Duration:* ${v.timestamp}
+*📤 Uploaded:* ${v.ago}
+*🔖 Viewes:* ${v.views} 
       `.trim()
       case 'channel': return `
 *${v.name}* (${v.url})
@@ -16,10 +18,10 @@ _${v.subCountLabel} (${v.subCount}) Subscriber_
 ${v.videoCount} video
 `.trim()
     }
-  }).filter(v => v).join('\n========================\n')
+  }).filter(v => v).join('\n▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n')
   m.reply(teks)
 }
-handler.help = ['ytsearch <pencarian>']
+handler.help = ['', 'earch'].map(v => 'yts' + v + ' <name>')
 handler.tags = ['internet']
 handler.command = /^yts(earch)?$/i
 
