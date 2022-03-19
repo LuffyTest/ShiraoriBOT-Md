9a2c554ac11b40b08f657e63abb459d83a685d72
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-19 13:57:47 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,17 +1,37 @@
-let { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys-md')
-let limit = 50
+let limit = 40
 const { servers, yta } = require('../lib/y2mate')
 let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+    if (!args || !args[0]) return m.reply('Uhm... where is the url?')
     let chat = global.db.data.chats[m.chat]
     let server = (args[1] || servers[0]).toLowerCase()
     let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    conn.reply(m.chat, wait, m)
-    if (!isLimit) await sock.sendMessage(m.chat, { document: { url: dl_link}, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, {quoted: m})
+    conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*🔮 Title:* ${title}
+*🔖 Filesize:* ${filesizeF}
+ ${isLimit ? 'This File Is Above Upload limit ': ''} Link:* ${dl_link}
+`.trim(), m)
+    if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*🔮 Title:* ${title}
+*🔖 Filesize:* ${filesizeF}
+`.trim(), m, null, {
+        asDocument: chat.useDocument
+    })
 }
-handler.help = ['ytmp3 <query>']
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
-handler.command = /^yt(a(udio)?|mp3|musik|lagu)$/i
+handler.command = /^(yta|ytmp3|mp3)$/i
+handler.owner = false
+handler.mods = false
+handler.limit = true
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
 
 module.exports = handler
