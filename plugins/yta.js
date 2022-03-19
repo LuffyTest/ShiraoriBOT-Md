281f12a9c9c1bb39d266ce7dc7c7221614454299
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-19 13:43:11 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,29 +1,28 @@
-let limit = 30
+let limit = 50
 const { servers, yta } = require('../lib/y2mate')
-let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return m.reply('Uhm... urlnya mana?')
-    let chat = global.db.data.chats[m.chat]
-    let server = (args[1] || servers[0]).toLowerCase()
-    let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... where is the url?'
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*🔮 Title:* ${title}
+*🔖 Filesize:* ${filesizeF}
+ ${isLimit ? 'This File Is Above Upload limit ': ''}
 `.trim(), m)
-    if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*🔮 Title:* ${title}
+*🔖 Filesize:* ${filesizeF}
 `.trim(), m, null, {
-        asDocument: chat.useDocument
-    })
+  asDocument: chat.useDocument
+})
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp3/tya'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
-handler.command = /^(yta|ytmp3|mp3)$/i
+handler.command = /^(yta||mp3)$/i
 handler.owner = false
 handler.mods = false
-handler.limit = false
 handler.premium = false
 handler.group = false
 handler.private = false
@@ -33,5 +32,6 @@ handler.botAdmin = false
 
 handler.fail = null
 handler.exp = 0
+handler.limit = false
 
 module.exports = handler
