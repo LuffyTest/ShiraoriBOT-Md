b7a0b6b4e5d3cc7475833eced3623ae784cc410e
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-21 11:43:56 +0530

    Create autorply-gm.js

---
+++ plugins/autorply-gm.js
@@ -0,0 +1,6 @@
+let handler = async m => m.reply(`Hello Good Morning 💕
+`.trim()) // Good Morning;
+handler.customPrefix [ /^(gm|Gm|GM|Goodmorning)$/i
+handler.command = new RegExp
+
+module.exports = handler
